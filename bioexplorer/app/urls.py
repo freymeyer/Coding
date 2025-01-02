347530ef32796f46dcd136b21a93e35bdf050ecb
freymeyer <coczeus2@gmail.com>
2025-01-02 16:46:56 +0800

    added counter and password maker

--- bioexplorer/app/urls.py
+++ bioexplorer/app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("todos/", views.todos, name="Todos")
+    path("todos/", views.todos, name="Todos"),
+    path("counter/", views.counter, name="Counter"),
+    path("password/", views.Password,name="password")
 ]
--- bioexplorer/app/views.py
+++ bioexplorer/app/views.py
@@ -8,3 +8,40 @@ def home(request):
 def todos(request):
     items = ToDoItem.objects.all()
     return render(request, "todos.html", {"todos":items})
+
+def counter(request):
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
+
+    if request.method == 'POST':
+        if 'increment' in request.POST:
+            request.session['counter'] += 100
+            print('Added counter')
+        elif 'decrement' in request.POST:
+            request.session['counter'] -= 100
+            print('Taken counter')
+        elif 'reset' in request.POST:
+            request.session['counter'] = 0
+            print('Reset counter')
+        request.session.modified =True
+    return render(request, "counter.html", {"counter": request.session['counter']})
+
+
+def Password(request):
+    if 'password' not in request.session:
+        request.session['password'] = ''
+    
+    if request.method == 'POST':
+        if 'alpha' in request.POST:
+            request.session['password'] += 'alpha'
+        elif 'beta' in request.POST:
+            request.session['password'] += 'beta'
+        elif 'cython' in request.POST:
+            request.session['password'] += 'cython'
+        elif 'reset' in request.POST:
+            request.session['password'] = ''
+        request.session.modified = True
+        print(request.session['password'])
+    return(render(request,"passwordmaker.html",{"password": request.session['password']}))        
+
+
